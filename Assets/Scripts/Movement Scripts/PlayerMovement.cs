8b389755b1b24d280a4fa07bda5366e529b5e14e
Arshnoor Singh <arshnoor88@gmail.com>
2024-08-21 13:01:32 +0530

    Jump almost functional

--- Assets/Scripts/Movement Scripts/PlayerMovement.cs
+++ Assets/Scripts/Movement Scripts/PlayerMovement.cs
@@ -8,14 +8,52 @@ public class PlayerMovement : MonoBehaviour
     Vector2 movementInput;
     public float movementSpeed = 0.1f;
 
+    bool jumpExecuted;
+    public float gravity = 9.8f;
+    public float jumpSpeed = 20;
+    public float verticalSpeed = 0;
+
     public void IAMovement(InputAction.CallbackContext context)
     {
         movementInput = context.ReadValue<Vector2>();
     }
 
+    public void IAJump(InputAction.CallbackContext context)
+    {
+        if(context.started == true)
+        {
+            jumpExecuted = true;
+            Debug.Log("IA Debug Jump = " + jumpExecuted);
+        }
+        else
+        {
+            jumpExecuted = false;
+            Debug.Log("IA Debug Jump = " + jumpExecuted);
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(movementInput.x * movementSpeed * Time.deltaTime, 0, movementInput.y * movementSpeed * Time.deltaTime);
+        verticalSpeed = (verticalSpeed - gravity) * Time.deltaTime;
+
+        if (GroundCheck() == true && jumpExecuted == false)
+        {
+            Debug.Log("Jump Condition Failed");
+            verticalSpeed = 0;
+        }
+
+        if(GroundCheck() == true && jumpExecuted == true)
+        {
+            Debug.Log("Jump Condition Successful");
+            verticalSpeed = jumpSpeed;
+        }
+
+        transform.Translate(movementInput.x * movementSpeed * Time.deltaTime, verticalSpeed, movementInput.y * movementSpeed * Time.deltaTime);
+    }
+
+    public bool GroundCheck()
+    {
+        return Physics.Raycast(transform.position, transform.up * -1, 1);
     }
 }
