6c7e3d397d41a0be1a906fe42c566ab96f11672b
Arshnoor Singh <arshnoor88@gmail.com>
2024-08-21 11:13:18 +0530

    Player Movement

---
+++ Assets/Scripts/Movement Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector2 movementInput;
+    public float movementSpeed = 0.1f;
+
+    public void IAMovement(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(movementInput.x * movementSpeed * Time.deltaTime, 0, movementInput.y * movementSpeed * Time.deltaTime);
+    }
+}
