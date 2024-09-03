9e462d33ce750490c44f23a9462f768fc681dc0d
Arshnoor Singh <arshnoor88@gmail.com>
2024-09-03 02:31:18 -0700

    Interaction Functional

---
+++ Assets/Scripts/Interaction/InteractionComponent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionComponent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// This function is executed whenever the player interacts with the gameObject that caontains this script as a component
+    /// </summary>
+    public void InteractionExecuted()
+    {
+        Debug.Log("Player interacted with " + gameObject.name);
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Movement Scripts/PlayerMovement.cs
+++ Assets/Scripts/Movement Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
     Vector2 movementInput;
     public float movementSpeed = 0.1f;
     public Cinemachine.CinemachineVirtualCamera playerCamera;
+    public float rayLength = 5f;
 
     public float gravity = 9.8f;
     public float jumpSpeed = 5;
@@ -54,12 +55,44 @@ public class PlayerMovement : MonoBehaviour
 
     public void InteractionRayCast()
     {
+        // Storing the value of the direction in which the camera is looking in a variable named CameraDirection
+        // The type of this variable is Vector3
         Vector3 CameraDirection = playerCamera.transform.forward;
 
-        Ray InteractionRay = new Ray(transform.position, CameraDirection);
+        // Storing the information about where our ray starts, and in which direction it needs to go, inside a variable called Interaction Ray
+        // The type of this variable is Ray
+        Ray InteractionRay = new Ray(playerCamera.transform.position, CameraDirection);
 
+        // Storing the information about whatever my raycast hits in a variable called targetInfo
+        // The type of this variable is RaycastHit
         RaycastHit targetInfo;
 
-        Physics.Raycast(InteractionRay, out targetInfo, 5f);
+        // Firing the ray using the information collected above
+        // The keyword out sends the data of whatever game object that has been hit by the raycast, into the variable name specified after the word out
+        // Obviously, the type of this variable needs to be RaycastHit
+        if(Physics.Raycast(InteractionRay, out targetInfo, rayLength) == true)
+        {
+            // Drawing a debug line to be able to see the ray for testing purposes
+            // This line is drawn if we hit something
+            Debug.DrawLine(playerCamera.transform.position, playerCamera.transform.position + (CameraDirection * rayLength), Color.red, 2f);
+
+            // Checking if the target info variable (which store the information about the object that was hit by the raycast) has a interaction component
+            if(targetInfo.transform.gameObject.GetComponent<InteractionComponent>() == true)
+            {
+                // Since the hit object contains the Interaction component, Execute the Intercation component function
+                targetInfo.transform.gameObject.GetComponent<InteractionComponent>().InteractionExecuted();
+            }
+            else
+            {
+                // If the hit object does not contain an interaction component, Simply print the following debug log
+                Debug.Log("Hit object does not conatain an interaction component");
+            }
+        }
+        else
+        {
+            // Drawing a debug line to be able to see the ray for testing purposes
+            // This line is drawn if we do not hit something
+            Debug.DrawLine(playerCamera.transform.position, playerCamera.transform.position + (CameraDirection * rayLength), Color.magenta, 2f);
+        }
     }
 }
